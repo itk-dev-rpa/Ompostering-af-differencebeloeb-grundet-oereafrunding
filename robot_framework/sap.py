b1bfee382a15ee792814970d29f6788b440fa43a
perska <perska@aarhus.dk>
2024-02-06 13:29:02 +0100

    Fixed whites spaces and added more comments to functions

--- robot_framework/sap.py
+++ robot_framework/sap.py
@@ -2,6 +2,7 @@
 
 from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConnection
 
+# Opens worklist
 def open_worklist(session):
     session.findById("wnd[0]/tbar[0]/okcd").text = "FPCRPO"
     session.findById("wnd[0]").sendVKey(0)
@@ -10,6 +11,7 @@ def open_worklist(session):
     # Press the search button
     session.findById("wnd[0]/tbar[1]/btn[8]").press()
 
+# Filters search results
 def filter_searches(session):
     # Press the 'Selekter' button
     session.findById("wnd[0]/tbar[1]/btn[33]").press()
@@ -17,6 +19,7 @@ def filter_searches(session):
     session.findById("wnd[1]/usr/ssubD0500_SUBSCREEN:SAPLSLVC_DIALOG:0501/cntlG51_CONTAINER/shellcont/shell").setCurrentCell(9,"TEXT")
     session.findById("wnd[1]/usr/ssubD0500_SUBSCREEN:SAPLSLVC_DIALOG:0501/cntlG51_CONTAINER/shellcont/shell").clickCurrentCell()
 
+# Formats the value of the field ForretnPartner
 def format_value(session):
     # Find the relevant value, which is in a wrong format
     unformatted = session.findById("wnd[0]/usr/subSUB1:SAPLFKCRPO:0100/subSUB_SALDO:SAPLFKCRPO:0107/txtFKKCRPO1-GPART_SALDO").text
@@ -33,7 +36,8 @@ def format_value(session):
     formatted = float(unformatted)
     return formatted
 
-def handle_case_or_skip(session, orchestrator_connection: OrchestratorConnection): 
+# Handles the relevant cases
+def handle_case_or_skip(session, orchestrator_connection: OrchestratorConnection):
     row_count = session.findById("wnd[0]/usr/cntlGRID1/shellcont/shell").rowCount
     # Open each case in the search result list
     for row in range(row_count):
@@ -47,12 +51,12 @@ def handle_case_or_skip(session, orchestrator_connection: OrchestratorConnection
             # Log message
             orchestrator_connection.log_info("Omposterer " + str(formatted))
             # Click 'Omposter Hovedbog'
-            session.findById("wnd[0]/usr/subSUB1:SAPLFKCRPO:0100/subSUB_FKT:SAPLFKCRPO:0108/tabsTAB_TRANSFER_POSTING/tabpGLTP").select() 
+            session.findById("wnd[0]/usr/subSUB1:SAPLFKCRPO:0100/subSUB_FKT:SAPLFKCRPO:0108/tabsTAB_TRANSFER_POSTING/tabpGLTP").select()
             # Insert the text 'diff'
             session.findById("wnd[0]/usr/subSUB1:SAPLFKCRPO:0100/subSUB_FKT:SAPLFKCRPO:0108/tabsTAB_TRANSFER_POSTING/tabpGLTP/ssubSUB_GL:SAPLFKCRPO:0120/ctxtFKKCRPO3-KUKON").text = "diff"
             # Click 'Omposter'
             session.findById("wnd[0]/usr/subSUB1:SAPLFKCRPO:0100/subSUB_FKT:SAPLFKCRPO:0108/tabsTAB_TRANSFER_POSTING/tabpGLTP/ssubSUB_GL:SAPLFKCRPO:0120/btnBTM_GL_TRANSF").press()
-        else: 
+        else:
             # Log message
             orchestrator_connection.log_info("Omposterer ikke " + str(formatted))
             # Click 'Gå tilbage til listen'
